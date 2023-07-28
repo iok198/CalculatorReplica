33ba2bbf15d367898a28cb2bf493a008309ab1b1
iok198 <leadDeveloper@warrensirota.com>
2023-07-27 22:43:31 -0400

    Initial Commit

---
+++ CalculatorReplica/CalculatorReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorReplicaApp.swift
+//  CalculatorReplica
+//
+//  Created by Isaac Okura on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
